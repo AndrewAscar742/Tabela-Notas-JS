a5fa7727c3ad0ed1b3382e419f7515842f73ad09
AndrewAscar742 <andrewmatosascar@outlook.com>
2022-08-15 19:53:42 -0300

    inicialização

---
+++ javaScript.js
@@ -0,0 +1,30 @@
+var nota1;
+var nota2;
+var nota3;
+var nota4;
+var media;
+
+function calcularNota1(){
+    nota1 = Number(document.getElementById('nota1').value)
+    nota2 = Number(document.getElementById('nota2').value)
+    nota3 = Number(document.getElementById('nota3').value)
+    nota4 = Number(document.getElementById('nota4').value)
+
+    media = (nota1 + nota2 + nota3 + nota4) / 4
+}
+
+function calcularNota2(){
+
+}
+
+function calcularNota3(){
+
+}
+
+function calcularNota4(){
+
+}
+
+function calcularNotasFinal(){
+
+}
