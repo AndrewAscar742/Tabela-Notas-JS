0b47ace7d89e89af61be5087ac54c069c1950b1e
AndrewAscar742 <andrewmatosascar@outlook.com>
2022-08-15 22:03:59 -0300

    Rumo a finalização

--- javaScript.js
+++ javaScript.js
@@ -1,30 +1,72 @@
-var nota1;
-var nota2;
-var nota3;
-var nota4;
-var media;
 
-function calcularNota1(){
-    nota1 = Number(document.getElementById('nota1').value)
-    nota2 = Number(document.getElementById('nota2').value)
-    nota3 = Number(document.getElementById('nota3').value)
-    nota4 = Number(document.getElementById('nota4').value)
+function calcularNota1() {
+    let nota1 = Number(document.getElementById('nota1').value)
+    let nota2 = Number(document.getElementById('nota2').value)
+    let nota3 = Number(document.getElementById('nota3').value)
+    let nota4 = Number(document.getElementById('nota4').value)
+    let media = (nota1 + nota2 + nota3 + nota4) / 4
+    document.getElementById('media1').innerText = media
 
-    media = (nota1 + nota2 + nota3 + nota4) / 4
+    if (media >= 0 && media < 50) {
+        document.getElementById('resultado1').innerText = "Reprovado"
+    } else {
+        if (media >= 50 && media < 70) {
+            document.getElementById('resultado1').innerText = "Recuperação"
+        } else {
+            document.getElementById('resultado1').innerText = "Aprovado"
+        }
+
+    }
 }
 
-function calcularNota2(){
+function calcularNota2() {
+    let nota1 = Number(document.getElementById('nota5').value)
+    let nota2 = Number(document.getElementById('nota6').value)
+    let nota3 = Number(document.getElementById('nota7').value)
+    let nota4 = Number(document.getElementById('nota8').value)
+    let media = (nota1 + nota2 + nota3 + nota4) / 4
+    document.getElementById('media2').innerText = media
 
+    if (media >= 0 && media < 50) {
+        document.getElementById('resultado2').innerText = "Reprovado"
+    } else {
+        if (media >= 50 && media < 70) {
+            document.getElementById('resultado2').innerText = "Recuperação"
+        } else {
+            document.getElementById('resultado2').innerText = "Aprovado"
+        }
+
+    }
 }
 
-function calcularNota3(){
+function calcularNota3() {
+    let nota1 = Number(document.getElementById('nota9').value)
+    let nota2 = Number(document.getElementById('nota10').value)
+    let nota3 = Number(document.getElementById('nota11').value)
+    let nota4 = Number(document.getElementById('nota12').value)
+    let media = (nota1 + nota2 + nota3 + nota4) / 4
+    document.getElementById('media3').innerText = media
 
+    if (media >= 0 && media < 50) {
+        document.getElementById('resultado3').innerText = "Reprovado"
+    } else {
+        if (media >= 50 && media < 70) {
+            document.getElementById('resultado3').innerText = "Recuperação"
+        } else {
+            document.getElementById('resultado3').innerText = "Aprovado"
+        }
+
+    }
 }
 
-function calcularNota4(){
 
-}
+function calcularMediaFinal() {
+    let nota1 = Number(document.getElementById('media1').value)
+    let nota2 = Number(document.getElementById('media2').value)
+    let nota3 = Number(document.getElementById('media3').value)
+    let media = (nota1 + nota2 + nota3) / 3
+    console.log(typeof(nota1) + typeof(nota2) + typeof(nota3))
+    console.log(nota1 + " " + nota2 + " " + nota3);
 
-function calcularNotasFinal(){
-
+    document.getElementById('mediaSala').innerText = media
 }
